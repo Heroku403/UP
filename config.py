e733605b1896efd852d3fa19bb0aa4d917399c62
Heroku403 <140687082+Heroku403@users.noreply.github.com>
2023-09-06 12:07:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5732190759:AAHQxfFlMtlmE_Scm3bpFldspzpbPtFu_UU")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5732190759:AAFjs0S17YrfEQjBI3YF0f16P9xnCzCSO6w")
     # The Telegram API things
     API_ID = int(os.environ.get("API_ID", 14136462))
     API_HASH = "54391f1d451d5223e26fc28ac6c86a67"
